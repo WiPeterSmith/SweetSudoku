47916ff8546e53d1fbe69e883672ebe54bf020fa
Peter Smith <wi.peter.smith@gmail.com>
2022-02-26 14:06:00 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    puzzle = [
+        [0, 4, 0, 7, 0, 6, 0, 0, 0], [0, 2, 0, 0, 5, 0, 0, 7, 3], [1, 7, 0, 9, 0, 3, 6, 0, 2],
+        [4, 0, 9, 1, 0, 0, 5, 0, 8], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ]
+    print(puzzle[0])
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+print_hi()
+print("Sweet sudoku project")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
