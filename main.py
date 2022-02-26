c1368fd56ae6a3de5fac6dd3a7fdec7e918a8058
Peter Smith <wi.peter.smith@gmail.com>
2022-02-26 15:32:34 -0600

    progress thus far

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import generator
+
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
-    puzzle = [
-        [0, 4, 0, 7, 0, 6, 0, 0, 0], [0, 2, 0, 0, 5, 0, 0, 7, 3], [1, 7, 0, 9, 0, 3, 6, 0, 2],
-        [4, 0, 9, 1, 0, 0, 5, 0, 8], [0, 0, 5, 7, 0, 0, 2, 1, 0], [2, 0, 0, 0, 9, 0, 4, 0, 0],
-        [9, 1, 0, 0, 8, 0, 2, 0, 4], [0, 0, 8, 0, 4, 2, 0, 9, 7], [0, 0, 4, 5, 0, 0, 3, 0, 0]
-    ]
-    for box in puzzle:
-        print(box)
+    puzzle = generator.generate()
+    print("Welcome to our Sudoku solver! Here is the current puzzle :)")
+    # for row in puzzle:
+    #     print(row)
+    solver(puzzle)
+
+
+def solver(puzzle):
+    print("doing stuff")
+    for row in puzzle:
+        for index in range(len(row)):
+            if row[index] == 0:
+                checkRow(row)
+                checkColumn(index, puzzle)
+                checkBox()
+
+            print('This is index {} with value {}'.format(index, row[index]))
+            # if value == 0:
+            #     print('This row {} at this value {}'.format(row, value))
+
+
+def checkRow(row):
+    for index in range(len(row)):
+        if index not in row:
+            return True
+    return False
 
 # if __name__ == '__main__':
-# main()
-print("I add here.")
-print("Sweet sudoku project")
+main()
 
-print("My dog has the shits")
