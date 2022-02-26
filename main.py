4ec19a850f55769cfa6f825984ddfcb537546812
Peter Smith <wi.peter.smith@gmail.com>
2022-02-26 14:27:45 -0600

    this is a test!

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi():
+def main():
     # Use a breakpoint in the code line below to debug your script.
     puzzle = [
         [0, 4, 0, 7, 0, 6, 0, 0, 0], [0, 2, 0, 0, 5, 0, 0, 7, 3], [1, 7, 0, 9, 0, 3, 6, 0, 2],
-        [4, 0, 9, 1, 0, 0, 5, 0, 8], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        [4, 0, 9, 1, 0, 0, 5, 0, 8], [0, 0, 5, 7, 0, 0, 2, 1, 0], [2, 0, 0, 0, 9, 0, 4, 0, 0],
+        [9, 1, 0, 0, 8, 0, 2, 0, 4], [0, 0, 8, 0, 4, 2, 0, 9, 7], [0, 0, 4, 5, 0, 0, 3, 0, 0]
     ]
-    print(puzzle[0])
+    for box in puzzle:
+        print(box)
 
-# Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
-print_hi()
+# main()
+print("I add here.")
 print("Sweet sudoku project")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
