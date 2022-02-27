aed79d778ab44cf9e8af23b1877c722f05de5ff4
Peter <wi.peter.smith@gmail>
2022-02-27 10:59:55 -0600

    I made empty array

--- generator.py
+++ generator.py
@@ -10,4 +10,11 @@ def generate():
         [0, 8, 0, 0, 4, 2, 5, 0, 0],
         [2, 0, 4, 0, 9, 7, 3, 0, 0]
     ]
+
+    array = [[i+1 for i in range(9)] for j in range(9)]
     return puzzle1
+
+
+# samplePuzzle = generate()
+# for arr in samplePuzzle:
+#     print(arr)
