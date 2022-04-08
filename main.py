98e1a11dce298506f35d90948c1500173a74dad9
Peter <wi.peter.smith@gmail>
2022-04-07 20:08:41 -0500

    Added GUI.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from pprint import pprint
 
+import PySimpleGUI as sg
 import generator
 
 
@@ -17,6 +18,23 @@ def main():
         solvedPuzzle = solver(solvedPuzzle)
 
     pprint(solvedPuzzle)
+
+    sg.theme('DarkAmber')  # Add a touch of color
+    # All the stuff inside your window.
+    layout = [[sg.Text('Some text on Row 1')],
+              [sg.Text('Enter something on Row 2'), sg.InputText()],
+              [sg.Button('Ok'), sg.Button('Cancel')]]
+
+    # Create the Window
+    window = sg.Window('Window Title', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+            break
+        print('You entered ', values[0])
+
+    window.close()
 
 
 # Returns puzzle completely solved if valid
